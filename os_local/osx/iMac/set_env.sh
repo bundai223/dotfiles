9c51d9d113c4eb431e3f1094403b50d1d40d40c8
bundai223 <bundai223@gmail.com>
2014-07-15 09:04:07 +0900

    moved environment_value_script to share directory.

--- os_local/osx/iMac/set_env.sh
+++
@@ -1,5 +0,0 @@
-# env setting script.
-launchctl setenv ANDROID_HOME /usr/local/opt/android-sdk
-launchctl setenv ECLIPSE_HOME /Applications/eclipse
-launchctl setenv GRADLE_HOME /usr/local/opt/gradle
-launchctl export
