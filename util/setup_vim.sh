8e81f45f284abe8300258d48ecb93a098b9a9f28
bundai223 <bundai223@gmail.com>
2014-07-24 17:16:57 +0900

    changed vim repos addr.

--- util/setup_vim.sh
+++ util/setup_vim.sh
@@ -4,7 +4,7 @@ sudo aptitude -y remove vim
 sudo aptitude -y install build-dep gettext tcl-dev lua5.2 liblua5.2-dev ruby-dev python3-dev
 
 # Get sourcecode.
-ghq get https://vim.googlecode.com/hg/vim
-cd ~/repos/vim.googlecode.com/hg/vim
+ghq get https://code.google.com/p/vim
+cd ~/repos/code.google.com/p/vim
 
 
