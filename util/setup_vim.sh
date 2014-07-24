f8e0f215da04fe1d9ede3c4d36bcb95ee7c2a293
bundai223 <bundai223@gmail.com>
2014-07-24 17:21:36 +0900

    setup configure

--- util/setup_vim.sh
+++ util/setup_vim.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
-sudo aptitude -y remove vim
+#sudo aptitude -y remove vim
 
 sudo aptitude -y install build-dep gettext tcl-dev lua5.2 liblua5.2-dev ruby-dev python3-dev
 
 # Get sourcecode.
-ghq get https://code.google.com/p/vim
-cd ~/repos/code.google.com/p/vim
+#ghq get https://code.google.com/p/vim
+#cd ~/repos/code.google.com/p/vim
 
+./configure --with-features=huge \
+--disable-selinux \
+--enable-largefile \
+--enable-luainterp \
+--with-luajit \
+--enable-perlinterp \
+--enable-pythoninterp \
+--enable-python3interp \
+--enable-tclinterp \
+--enable-rubyinterp \
+--enable-cscope \
+--enable-multibyte \
+--enable-gui \
+--enable-fail-if-missing
 
