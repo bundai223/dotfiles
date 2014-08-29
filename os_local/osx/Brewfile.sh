47762b892eb8f21db994e1c7d96f8096a7b17319
bundai223 <bundai223@gmail.com>
2014-08-29 19:01:44 +0900

    hoge

--- os_local/osx/Brewfile.sh
+++ os_local/osx/Brewfile.sh
@@ -39,59 +39,62 @@ if [[ $OSTYPE == darwin* ]]; then
     brew install --HEAD ctags-objc-ja
     brew install macvim-kaoriya --HEAD --with-lua --with-cscope
     brew linkapps
+    ln -s /usr/local/opt/macvim/MacVim.app ~/Applications/
+
+    # for hobby
+    if [ $OSX_PRIVATE ]; then
+        echo "nothing to do."
+    fi
+
 else
     brew install vim --with-lua --with-luajit
-fi
-
-# for hobby
-if [ $OSX_PRIVATE ]; then
-    echo "nothing to do."
 fi
 
 brew cleanup
 
 # cask
-brew tap caskroom/homebrew-cask
-brew install brew-cask
-brew cask update
+if [[ $OSTYPE == darwin* ]]; then
+    brew tap caskroom/homebrew-cask
+    brew install brew-cask
+    brew cask update
 
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install xtrafinder
-brew cask install dash
-brew cask install android-file-transfer
-brew cask install android-studio
-brew cask install intellij-idea-ce
-brew cask install atom
-brew cask install sourcetree
-brew cask install genymotion
-brew cask install sequel-pro
-brew cask install rcdefaultapp
-brew cask install virtualbox
-brew cask install synergy
-brew cask install vlc
-brew cask install menumeters
-#brew cask install cheatsheet
+    brew cask install google-chrome
+    brew cask install iterm2
+    brew cask install xtrafinder
+    brew cask install dash
+    brew cask install android-file-transfer
+    brew cask install android-studio
+    brew cask install intellij-idea-ce
+    brew cask install atom
+    brew cask install sourcetree
+    brew cask install genymotion
+    brew cask install sequel-pro
+    brew cask install rcdefaultapp
+    brew cask install virtualbox
+    brew cask install synergy
+    brew cask install vlc
+    brew cask install menumeters
+    #brew cask install cheatsheet
 
-# for hobby
-if [ $OSX_PRIVATE ]; then
-    brew cask install skype
-    brew cask install xquartz
-    brew cask install xbox360-controller-driver
-    brew cask install blender
-    brew cask install gimp
-    brew cask install inkscape
-    brew cask install minecraft
-    brew cask install steam
-    brew cask install dropbox
-    brew cask install cooviewer
-#    mediahuman
-#    mikutter
-#    snes
-#    grafittipod
+    # for hobby
+    if [ $OSX_PRIVATE ]; then
+        brew cask install skype
+        #brew cask install wireshark
+        brew cask install xquartz
+        brew cask install xbox360-controller-driver
+        brew cask install blender
+        brew cask install gimp
+        brew cask install inkscape
+        brew cask install minecraft
+        brew cask install steam
+        brew cask install dropbox
+        brew cask install cooviewer
+    #    mediahuman
+    #    mikutter
+    #    snes
+    #    grafittipod
+    fi
+
+    brew cask cleanup
 fi
-
-brew cask cleanup
-
-ln -s /usr/local/opt/macvim/MacVim.app ~/Applications/
 
