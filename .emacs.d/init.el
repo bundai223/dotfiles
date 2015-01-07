eb6adb889bd9bfe06c73fd3182ec8e5d6afa8ece
bundai223 <bundai223@gmail.com>
2015-01-07 19:25:37 +0900

    emacsのPowerline導入

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,8 +4,11 @@
 
 ;; package management
 (require 'package)
-(add-to-list 'package-archives
-                          '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+;; initialize
 (package-initialize)
 
 (defun package-install-with-refresh (package)
@@ -16,8 +19,11 @@
 
 ;; install evil
 (package-install-with-refresh 'evil)
+(package-install-with-refresh 'powerline)
 
 ;; enable evil
 (require 'evil)
 (evil-mode 1)
 
+(require 'powerline)
+(powerline-default-theme)
