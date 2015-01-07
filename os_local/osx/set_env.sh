8164925bd01bd787069351efff703bb1da90a590
bundai223 <bundai223@gmail.com>
2015-01-07 11:34:01 +0900

    dfa

--- os_local/osx/set_env.sh
+++ os_local/osx/set_env.sh
@@ -2,4 +2,4 @@
 launchctl setenv ANDROID_HOME /usr/local/opt/android-sdk
 launchctl setenv ECLIPSE_HOME /Applications/eclipse
 launchctl setenv GRADLE_HOME /usr/local/opt/gradle
-launchctl export
+#launchctl export
