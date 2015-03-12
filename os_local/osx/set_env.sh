7c82c562c1607c8654217ab1c88f34df7abc83da
bundai223 <bundai223@gmail.com>
2015-03-12 19:28:14 +0900

    GO関係のパスをGuiツールでも使用できるようにスクリプトに追加

--- os_local/osx/set_env.sh
+++ os_local/osx/set_env.sh
@@ -2,4 +2,7 @@
 launchctl setenv ANDROID_HOME /usr/local/opt/android-sdk
 launchctl setenv ECLIPSE_HOME /Applications/eclipse
 launchctl setenv GRADLE_HOME /usr/local/opt/gradle
+
+launchctl setenv GOROOT /usr/local/opt/go/libexec
+launchctl setenv GOPATH $HOME/.go
 #launchctl export
