4c9d304d515403fcd3fc6254e0f8b52f503fe6ae
bundai223 <bundai223@gmail.com>
2015-07-24 19:07:51 +0900

    hgoe

--- os_local/osx/Brewfile.sh
+++ os_local/osx/Brewfile.sh
@@ -4,76 +4,77 @@ brew update
 # cask
 # gui tools
 if [[ $OSTYPE == darwin* ]]; then
-    # brew tap caskroom/versions
-    brew tap bundai223/personal
+  # brew tap caskroom/versions
+  brew tap bundai223/personal
 
-    brew install caskroom/cask/brew-cask
-    brew cask update
+  brew install caskroom/cask/brew-cask
+  brew cask update
 
-    brew cask install kobito
-    brew cask install xquartz
-    brew cask install google-chrome
-    brew cask install iterm2
-    brew cask install xtrafinder
-    brew cask install dash
-    brew cask install android-file-transfer
-    brew cask install android-studio
-    brew cask install atom
-    brew cask install sourcetree
-    brew cask install genymotion
-#     brew cask install rcdefaultapp
-    brew cask install virtualbox
-    brew cask install vagrant
-    brew cask install vlc
-#     brew cask install menumeters
-    brew cask install hyperswitch
-#     brew cask install appcleaner
-    brew cask install fluid
-    brew cask install platypus
-    brew cask install cd-to
-    brew cask install karabiner
+  caskapp=(
+    kobito
+    xquartz
+    google-chrome
+    iterm2
+    xtrafinder
+    dash
+    android-file-transfer
+    android-studio
+    atom
+    sourcetree
+    genymotion
+    virtualbox
+    vagrant
+    vlc
+    menumeters
+    hyperswitch
+    appcleaner
+    fluid
+    platypus
+    cd-to
+    karabiner
+    java
+  )
 
-    #brew install caskroom/versions/java6
-
-    # for hobby
-    if [ $OSX_PRIVATE ]; then
-        brew cask install 1password dropbox
-        brew cask install skype
-        brew cask install xbox360-controller-driver
-        brew cask install blender
-        brew cask install gimp
-        brew cask install inkscape
-#         brew cask install minecraft
-        brew cask install steam
-        brew cask install cooviewer
-        brew cask install calibre
-        brew cask install dolphin openemu
-        brew cask install aquaskk
+  # for hobby
+  if [ $PRIVATE ]; then
+    caskapp=(
+      "${caskapp}"
+      1password
+      dropbox
+      skype
+      xbox360-controller-driver
+      blender
+      gimp
+      inkscape
+      minecraft
+      steam
+      cooviewer
+      calibre
+      dolphin
+      openemu
+      aquaskk
+    )
     #    mediahuman
     #    mikutter
     #    snes
     #    grafittipod
-    fi
+  fi
 
-    brew cask cleanup
+  brew cask install --appdir="/Applications" ${caskapp[@]}
+  brew cask cleanup
 fi
 
 # cui tools
-# brew install jenkins
-# brew install nodebrew
-# brew install mecab
-# brew install node redis
-
-brew install wget coreutils
+brew install wget coreutils findutils
 brew install plantuml
 
 # program languages.
 brew install global
-brew install haxe  # altJs languages.
+# brew install haxe  # altJs languages.
 brew install rust
-brew install go --cross-compile-common
+# brew install go --cross-compile-common
 brew install luajit lua
-brew install ruby
+# brew install ruby
 brew install python
 brew install opam rlwrap
 
@@ -81,33 +82,31 @@ brew install opam rlwrap
 brew install git mercurial
 brew install z zsh tmux
 brew install cmake
-brew install android-sdk android-ndk ant apktool
+# brew install android-sdk android-ndk ant apktool
 brew install the_silver_searcher
 brew install docker boot2docker
 # brew install emacs --cocoa --with-gnutls
 
 brew install sanemat/font/ricty --vim-powerline
-# brew install homebrew/completions/rustc-completion
-# brew install homebrew/completions/cargo-completion
 
 if [[ $OSTYPE == darwin* ]]; then
-    brew install terminal-notifier
-    brew install reattach-to-user-namespace
+  brew install terminal-notifier
+  brew install reattach-to-user-namespace
 
-    # macvim-kaoriya
-    brew install cscope
-    brew install KazuakiM/macvim/macvim-kaoriya --HEAD --with-lua --with-cscope
-    brew linkapps
-    ln -s /usr/local/opt/macvim/MacVim.app ~/Applications/
+  # macvim-kaoriya
+  brew install cscope
+  brew install KazuakiM/macvim/macvim-kaoriya --HEAD --with-lua --with-cscope
+  brew linkapps
+  ln -s /usr/local/opt/macvim/MacVim.app ~/Applications/
 
-    # for hobby
-    if [ $OSX_PRIVATE ]; then
-        echo "nothing to do."
-    fi
+  # for hobby
+  if [ $PRIVATE ]; then
+    echo "nothing to do."
+  fi
 
 else
-    brew install ctags
-    brew install vim --with-lua --with-luajit
+  brew install ctags
+  brew install vim --with-lua --with-luajit
 fi
 
 brew cleanup
