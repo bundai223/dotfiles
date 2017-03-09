125a59fbfa797cd8bf230af7bb9b895f67f397b8
bundai223 <bundai223@gmail.com>
2017-03-09 16:51:11 +0900

    neovimを最新にする処理を追加

--- util/centos/install_neovim.sh
+++ util/centos/install_neovim.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-BASEPATH=/usr/src
 sudo yum -y install libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
 
+# BASEPATH=/usr/src
+BASEPATH=~/
 cd $BASEPATH
-git clone https://github.com/neovim/neovim.git
-cd neovim
+src_dir=$BASEPATH/neovim_src
+if [ ! -e $src_dir ]; then
+  git clone https://github.com/neovim/neovim.git $src_dir
+fi
+cd $src_dir
+git checkout .
+git clean -f .
 if [ -e build ]; then
   rm -r build
 fi
 make clean
 make CMAKE_BUILD_TYPE=Release
-make && make install
+make && sudo make install
