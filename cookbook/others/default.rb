23385fa32d32cce18d72f5553bd76449963a775b
Daiji Nishimura <daiji@DaijinoMacBook-puro.local>
2018-04-18 01:04:51 +0900

    install

---
+++ cookbook/others/default.rb
@@ -0,0 +1,28 @@
+
+
+# osx
+package 'wget'
+package 'coreutils'
+package 'findutils'
+package 'luajit'
+package 'lua'
+package 'git'
+package 'mercurial'
+package 'z'
+package 'the_silver_searcher'
+package 'reattach-to-user-namespace'
+# cask
+package 'caskroom/cask/google-chrome'
+package 'caskroom/cask/android-file-transfer'
+package 'caskroom/cask/1password'
+package 'caskroom/cask/buttercup'
+package 'caskroom/cask/dropbox'
+package 'caskroom/cask/skype'
+package 'caskroom/cask/steam'
+package 'caskroom/cask/cooviewer'
+package 'caskroom/cask/vlc'
+package 'caskroom/cask/iterm2'
+package 'caskroom/cask/dash'
+package 'caskroom/cask/android-studio'
+package 'caskroom/cask/virtualbox'
+package 'caskroom/cask/vagrant'
--- cookbook/rust/default.rb
+++ cookbook/rust/default.rb
@@ -3,3 +3,16 @@ execute 'install rustup' do
   command 'curl https://sh.rustup.rs -sSf | sh'
 end
 
+execute 'install cargo tools' do
+  command <<-EOL
+    source ~/.cargo/env
+    cargo install racer
+    cargo install rustfmt
+  EOL
+end
+
+execute 'get rust src' do
+  command <<-EOL
+    rustup component add rust-src
+  EOL
+end
