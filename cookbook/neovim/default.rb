dad5e0a787fde30fdd67564519de3419cfefa238
bundai223 <bundai223@gmail.com>
2018-04-25 10:37:52 +0900

    update neovim install

--- cookbook/neovim/default.rb
+++ cookbook/neovim/default.rb
@@ -12,7 +12,44 @@ when 'debian', 'ubuntu', 'mint'
   package 'python3-pip'
   package 'neovim'
 
-when 'fedora', 'redhat', 'amazon'
+when 'fedora', 'redhat'
+  package 'epel-release'
+
+  execute 'add yum repository' do
+    command <<-EOL
+      curl -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo
+    EOL
+  end
+
+  package 'neovim'
+
+when 'amazon'
+  package 'libtool'
+  package 'autoconf'
+  package 'automake'
+  package 'cmake'
+  package 'gcc'
+  package 'gcc-c++'
+  package 'make'
+  package 'pkgconfig'
+  package 'unzip'
+
+  execute 'install neovim' do
+    command <<-EOL
+      BASEPATH=/usr/src
+      cd $BASEPATH
+
+      git clone https://github.com/neovim/neovim.git
+      cd neovim
+      if [ -e build ]; then
+        rm -r build
+      fi
+      make clean
+      make CMAKE_BUILD_TYPE=Release
+      make && sudo make install
+    EOL
+  end
+
 when 'osx', 'darwin'
   package 'nvim'
 when 'arch'
