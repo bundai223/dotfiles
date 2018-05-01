6eeae47e18f28a3ff52b601c71699ba867f8ae1d
bundai223 <bundai223@gmail.com>
2018-05-01 16:58:21 +0900

    newest zsh on ubuntu

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -549,7 +549,20 @@ function! MyCharCode()
   return "'". char ."' ". nr
 endfunction
 
-
+if system('uname -a | grep Microsoft') != ""
+  let g:clipboard = {
+        \   'name': 'myClipboard',
+        \   'copy': {
+        \      '+': 'win32yank.exe -i',
+        \      '*': 'win32yank.exe -i',
+        \    },
+        \   'paste': {
+        \      '+': 'win32yank.exe -o',
+        \      '*': 'win32yank.exe -o',
+        \   },
+        \   'cache_enabled': 1,
+        \ }
+endif
 
 if has('unix')
   if !has('gui_running')
--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -1,8 +1,30 @@
 
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
-  package 'zsh'
+  package 'wget'
 
+  execute 'compile zsh' do
+    command <<-EOL
+      VERSION=5.5.1
+      WORKDIR=work_zsh
+
+      cur=$(pwd)
+      mkdir -p ${WORKDIR}
+      cd ${WORKDIR}
+
+      wget -O zsh-${VERSION}.tar.gz http://sourceforge.net/projects/zsh/files/zsh/${VERSION}/zsh-${VERSION}.tar.gz/download
+      tar fx zsh-${VERSION}.tar.gz
+      cd zsh-${VERSION}
+      ./configure --prefix=/usr/local
+      make
+      sudo make install
+
+      cd ${cur}
+      rm -rf ${WORKDIR}
+    EOL
+
+    not_if 'test -e /usr/local/bin/zsh'
+  end
 when 'fedora', 'redhat', 'amazon'
   package 'wget'
   package 'ncurses-devel'
@@ -10,28 +32,28 @@ when 'fedora', 'redhat', 'amazon'
   #!/bin/bash
   # VERSION=5.4.2
   # WORKDIR=work_zsh
-  # 
+  #
   # install_zsh()
   # {
   #   which zsh 2>/dev/null 1>$2 && return
-  # 
+  #
   #   sudo yum -y install wget ncurses-devel
-  # 
+  #
   #   cur=$(pwd)
   #   mkdir -p ${WORKDIR}
   #   cd ${WORKDIR}
-  # 
+  #
   #   wget -O zsh-${VERSION}.tar.gz http://sourceforge.net/projects/zsh/files/zsh/${VERSION}/zsh-${VERSION}.tar.gz/download
   #   tar fx zsh-${VERSION}.tar.gz
   #   cd zsh-${VERSION}
   #   ./configure --prefix=/usr
   #   make
   #   sudo make install
-  # 
+  #
   #   cd ${cur}
   #   rm -rf ${WORKDIR}
   # }
-  # 
+  #
   # install_zsh
 
 when 'osx', 'darwin'
