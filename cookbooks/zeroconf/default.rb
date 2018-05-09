abeae212faa59f56ca7d6bb8671893e5c3a76345
bundai223 <bundai223@gmail.com>
2018-05-09 14:17:05 +0900

    zeroconf

--- cookbooks/zeroconf/default.rb
+++ cookbooks/zeroconf/default.rb
@@ -1,3 +1,23 @@
-package 'avahi'
-package 'nss-mdns'
 
+case node[:platform]
+when 'debian', 'ubuntu', 'mint'
+  package 'avahi-daemon'
+
+  # for wsl
+  service 'dbus' do
+    action [:start, :enable]
+    not_if 'uname -a | grep Microsoft'
+  end
+when 'fedora', 'redhat', 'amazon'
+  package 'avahi'
+  package 'nss-mdns'
+when 'osx', 'darwin'
+when 'arch'
+when 'opensuse'
+else
+end
+
+service 'avahi-daemon' do
+  action [:start, :enable]
+  not_if 'uname -a | grep Microsoft'
+end
