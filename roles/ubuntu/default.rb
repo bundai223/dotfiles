f60f2ad78135844b4e7e8c09cae7368da9395be6
bundai223 <bundai223@gmail.com>
2018-05-11 19:54:42 +0900

    ubuntu(wsl)用にx11のインストール追加

--- roles/ubuntu/default.rb
+++ roles/ubuntu/default.rb
@@ -2,15 +2,22 @@ include_role('base')
 
 # for nokogiri for rails
 package 'build-essential'
+package 'apt-file'
+package 'sysv-rc-conf'
+
 package 'patch'
 package 'ruby-dev'
 package 'zlib1g-dev'
 package 'liblzma-dev'
 package 'libxml2-dev'
 package 'python-fontforge'
-package 'apt-file'
+
 
 # locale
 #package 'language-pack-ja'
 
+package 'x11-apps'
+package 'x11-utils'
+package 'x11-xserver-utils'
+package 'fonts-ipafont'
 execute 'locale-gen ja_JP.UTF-8'
