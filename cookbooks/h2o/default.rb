6c1c88fbbcdef76d1a595dbe974cf376552ae329
bundai223 <bundai223@gmail.com>
2018-05-18 13:31:45 +0900

    add h2o

---
+++ cookbooks/h2o/default.rb
@@ -0,0 +1,40 @@
+
+case node[:platform]
+when 'debian', 'ubuntu', 'mint'
+  # dependent cookbook
+  #include_cookbook 'git'
+  #include_cookbook 'ghq'
+
+  package 'locate'
+  package 'cmake'
+  package 'build-essential'
+  package 'checkinstall'
+  package 'autoconf'
+  package 'pkg-config'
+  package 'libtool'
+  package 'python-sphinx'
+  package 'wget'
+  package 'libcunit1-dev'
+  package 'nettle-dev'
+  package 'libyaml-dev'
+  package 'libuv-dev'
+
+  execute "#{sudo(node[:user])}ghq get h2o/h2o"
+  execute 'install h2o' do
+    command <<-EOL
+      cd repos/github.com/h2o/h2o
+      cmake -DWITH_BUNDLED_SSL=on .
+      make
+      make install
+    EOL
+
+    not_if 'test -e /usr/local/bin/h2o'
+  end
+
+when 'fedora', 'redhat', 'amazon'
+  package 'git-flow'
+when 'osx', 'darwin'
+when 'arch'
+when 'opensuse'
+else
+end
