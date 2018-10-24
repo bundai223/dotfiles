7b0045e60acfe8464ccb5c2a576cdd0adce910c3
bundai223 <bundai223@gmail.com>
2018-10-24 22:07:59 +0900

    update

--- cookbooks/nodejs/default.rb
+++ cookbooks/nodejs/default.rb
@@ -1,22 +1,29 @@
 include_recipe 'dependency.rb'
 
 node.reverse_merge!({
+  nvm: {
+    version: 'v0.33.11',
+  },
   nodejs: {
     major_version: 8,
   }
 })
 
 major_version = node[:nodejs][:major_version]
+nvm_version = node[:nvm][:version]
 
 case node[:platform]
 when 'debian', 'ubuntu', 'mint'
-  execute "install nodejs#{major_version}" do
-    command <<-EOL
-      curl -sL https://deb.nodesource.com/setup_#{major_version}.x | sudo -E bash -
-    EOL
+  execute 'nvm install' do
+    command "curl -o- https://raw.githubusercontent.com/creationix/nvm/#{nvm_version}/install.sh | bash"
   end
-
-  package 'nodejs'
+  # execute "install nodejs#{major_version}" do
+  #   command <<-EOL
+  #     curl -sL https://deb.nodesource.com/setup_#{major_version}.x | sudo -E bash -
+  #   EOL
+  # end
+  #
+  # package 'nodejs'
 
 when 'fedora', 'redhat', 'amazon'
   execute "install nodejs#{major_version}" do
