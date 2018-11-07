6b5d72ec45cca9c13d231816c99d3b06d12467bf
bundai223 <bundai223@gmail.com>
2018-11-07 18:33:45 +0900

    vscode対応

---
+++ cookbooks/vscode/default.rb
@@ -0,0 +1,25 @@
+# https://code.visualstudio.com/docs/setup/linux
+
+case node[:platform]
+when 'arch'
+when 'osx', 'darwin'
+when 'fedora', 'redhat', 'amazon'
+when 'debian', 'ubuntu', 'mint'
+  execute 'add vscode repos' do
+    command <<-EOL
+      curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+      sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+      sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+      # おそうじ
+      rm microsoft.gpg
+    EOL
+  end
+
+  package 'apt-transport-https'
+  execute 'apt update -y'
+  package 'code'
+
+when 'opensuse'
+else
+end
