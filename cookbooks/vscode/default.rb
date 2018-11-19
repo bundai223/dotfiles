89fafbb9c9fc9352d9b3892fe0b1a7c917d69bd1
bundai223 <bundai223@gmail.com>
2018-11-19 18:11:35 +0900

    add vscode install

--- cookbooks/vscode/default.rb
+++ cookbooks/vscode/default.rb
@@ -2,6 +2,9 @@
 
 case node[:platform]
 when 'arch'
+  include_cookbook 'yay'
+  yay 'visual-studio-code-bin'
+
 when 'osx', 'darwin'
 when 'fedora', 'redhat', 'amazon'
 when 'debian', 'ubuntu', 'mint'
