fbbb7f4669a2e39b8ecb6f2cb21bfa02e8985a2b
bundai223 <bundai223@gmail.com>
2018-12-14 00:08:53 +0900

    user install pip

--- cookbooks/neovim/dependency.rb
+++ cookbooks/neovim/dependency.rb
@@ -2,12 +2,18 @@ include_cookbook 'ruby'
 include_cookbook 'python'
 include_cookbook 'yarn'
 
+package 'cmake'
+
 # ruby
 gem_package 'neovim'
 
 # python
-execute 'pip3 install --upgrade neovim'
-execute 'pip2 install --upgrade neovim'
+execute 'pip3 install --upgrade --user neovim' do
+  user node[:user]
+end
+execute 'pip2 install --upgrade --user neovim' do
+  user node[:user]
+end
 
 # Node.js
 execute 'yarn global add neovim'
