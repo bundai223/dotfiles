a09b3816975a4818c2b517a30f893ab53cc5d50d
Daiji Nishimura <bundai223@gmail.com>
2019-02-22 13:22:14 +0900

    Update default.rb

--- roles/arch/default.rb
+++ roles/arch/default.rb
@@ -2,17 +2,16 @@
 # The tmpfs can also be temporarily resized without the need to reboot, for example when a large compile job needs to run soon. In this case, run:
 execute 'mount -o remount,size=4G,noatime /tmp'
 
-execute 'pacman -Syy'
+include_cookbook('yay')
+
+update_package
+
 package 'base-devel'
 package 'openssh'
 package 'libxml2'
 package 'libxslt'
 
 include_role('base')
-
-include_cookbook('yay')
-
-update_package
 
 # yay 'buttercup-desktop'
 # yay 'dropbox'
