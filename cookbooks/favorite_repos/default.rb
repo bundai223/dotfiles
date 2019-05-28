dc9b52f23fe28b09af7dd9b2f07e29b182123cd5
bundai223 <bundai223@gmail.com>
2019-05-28 12:56:44 +0900

    add hackgen

--- cookbooks/favorite_repos/default.rb
+++ cookbooks/favorite_repos/default.rb
@@ -2,11 +2,12 @@ include_cookbook 'dotfiles'
 include_cookbook 'ghq'
 
 repos = [
+  'dylanaraps/neofetch',
   'mzyy94/RictyDiminished-for-Powerline',
+  'rupa/z',
+  'yuru7/HackGen',
   'zsh-users/zsh-completions',
   'zplug/zplug',
-  'rupa/z',
-  'dylanaraps/neofetch'
 ]
 repos.each { |name| get_repo name }
 
