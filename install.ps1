6e1d83d4f0a13df02aedf025cedacf20eaf0313b
Daiji Nishimura <bundai223@gmail.com>
2019-10-08 10:08:42 +0900

    Create install.ps1
    Add Basic install script for windows.

---
+++ install.ps1
@@ -0,0 +1,15 @@
+#Requires -Version 5
+
+# install scoop
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+$buckets = 'extras'
+$apps = 'googlechrome',
+        'brave'
+  
+# add bucekts
+$buckets | % { scoop bucket add $_ }
+
+# install app
+$a = $apps -join ' '
+scoop install googlechrome $a
