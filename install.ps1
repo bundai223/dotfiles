d651edc113214f83a3665b9819eb077e39c4c876
Daiji Nishimura <bundai223@gmail.com>
2019-11-18 16:33:58 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -9,7 +9,8 @@ $known_buckets = @(
 $unknown_buckets = @(
   @{name = 'bundai223'; url = 'https://github.com/bundai223/scoop-for-jp'},
   @{name = 'anurse'; url = 'https://github.com/anurse/scoop-bucket'}, # 
-  @{name = 'sh4869221b'; url = 'https://github.com/sh4869221b/scoop-bucket'} # virtualbox
+  @{name = 'sh4869221b'; url = 'https://github.com/sh4869221b/scoop-bucket'}, # virtualbox
+  @{name = 'wangzq'; url = 'https://github.com/wangzq/scoop-bucket'} # powershell-core
 )
 $apps = @(
   'googlechrome',
