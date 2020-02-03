9cc73014aec33d184a7ec27fb0ed88814b33224b
Daiji NISHIMURA <bundai223@gmail.com>
2020-02-04 02:38:25 +0900

    update comment

--- cookbooks/nvidia/default.rb
+++ cookbooks/nvidia/default.rb
@@ -2,6 +2,7 @@ include_recipe 'dependency.rb'
 
 case node[:platform]
 when 'arch'
+  # https://wiki.manjaro.org/index.php?title=Configure_NVIDIA_(non-free)_settings_and_load_them_on_Startup
   execute 'mhwd -a pci nonfree 0300'
 when 'osx', 'darwin'
 when 'fedora', 'redhat', 'amazon'
