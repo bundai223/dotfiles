962d303754ae5becb7e545927a045e4b99011d86
bundai223 <bundai223@gmail.com>
2021-08-26 02:00:48 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -76,6 +76,7 @@ mkdir -Force -p "$HOME\scoop\persist\keypirinha\portable\Profile"
 New-Item -Value "$HOME\repos\github.com\bundai223\dotfiles\config\WindowsTerminal\settings.json" -Path "$HOME\AppData\Local\Microsoft\Windows Terminal" -Name settings.json -ItemType SymbolicLink
 New-Item -Value "$HOME\repos\github.com\bundai223\dotfiles\config\Microsoft.PowerShell_profile.ps1" -Path "$HOME\Documents\PowerShell" -Name Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink
 New-Item -Value "$HOME\repos\github.com\bundai223\dotfiles\config\keypirinha\Profile\User" -Path "$HOME\scoop\persist\keypirinha\portable\Profile" -Name User -ItemType SymbolicLink
+New-Item -Value "$HOME\repos\github.com\bundai223\dotfiles\config\.gitconfig" -Path "$HOME\" -Name .gitconfig -ItemType SymbolicLink
 
 # Storeアプリインストールしてちょ
 echo '* Please install store apps.'
