05eae2ae99dcdb792056f591af0d43ece992f095
bundai223 <bundai223@gmail.com>
2022-06-02 13:09:37 +0900

    which-keysを導入

--- config/.config/nvim/lua/plugins.lua
+++ config/.config/nvim/lua/plugins.lua
@@ -408,6 +408,16 @@ return require('packer').startup(function(use)
         extensions = {
         }
       })
+
+      -- nmap <C-u> [denite]
+      vim.api.nvim_set_keymap("n", "[FuzzyFinder]", "<Nop>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("v", "[FuzzyFinder]", "<Nop>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "z", "[FuzzyFinder]", {})
+      vim.api.nvim_set_keymap("v", "z", "[FuzzyFinder]", {})
+      vim.api.nvim_set_keymap("n", "[FuzzyFinder]f", "<Cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "[FuzzyFinder]b", "<Cmd>Telescope buffers<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "[FuzzyFinder]p", "<Cmd>Telescope git_files<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "[FuzzyFinder]m", "<Cmd>Telescope frecency<CR>", { noremap = true, silent = true })
     end,
   })
   use({
@@ -570,4 +580,63 @@ return require('packer').startup(function(use)
           "MunifTanjim/nui.nvim",
         }
       }
+      --------------------------------
+      -- Manual
+      use({
+        "thinca/vim-ref",
+        event = "VimEnter",
+        config = function()
+          -- vim.cmd("source ~/.config/nvim/rc/pluginconfig/vim-ref.vim")
+        end,
+      })
+      use({
+        "folke/which-key.nvim",
+        event = "VimEnter",
+        config = function()
+          -- require("rc/pluginconfig/which-key")
+          require("which-key").setup({
+            plugins = {
+              marks = false, -- shows a list of your marks on ' and `
+              registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+              -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+              -- No actual key bindings are created
+              presets = {
+                operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+                motions = false, -- adds help for motions
+                text_objects = false, -- help for text objects triggered after entering an operator
+                windows = false, -- default bindings on <c-w>
+                nav = false, -- misc bindings to work with windows
+                z = false, -- bindings for folds, spelling and others prefixed with z
+                g = false, -- bindings for prefixed with g
+              },
+            },
+            -- add operators that will trigger motion and text object completion
+            -- to enable all native operators, set the preset / operators plugin above
+            operators = { gc = "Comments" },
+            icons = {
+              breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+              separator = "➜", -- symbol used between a key and it's label
+              group = "+", -- symbol prepended to a group
+            },
+            window = {
+              border = "none", -- none, single, double, shadow
+              position = "bottom", -- bottom, top
+              margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+              padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+            },
+            layout = {
+              height = { min = 4, max = 25 }, -- min and max height of the columns
+              width = { min = 20, max = 50 }, -- min and max width of the columns
+              spacing = 3, -- spacing between columns
+            },
+            hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+            show_help = true, -- show help message on the command line when the popup is visible
+            -- triggers = "auto", -- automatically setup triggers
+            triggers = { "<Leader>" }, -- or specify a list manually
+          })
+
+          vim.api.nvim_set_keymap("n", "<Leader><CR>", "<Cmd>WhichKey \\ <CR>", { noremap = true })
+          vim.api.nvim_set_keymap("n", "[FuzzyFinder]<CR>", "<Cmd>WhichKey [FuzzyFinder]<CR>", { noremap = true })
+        end,
+      })
     end)
