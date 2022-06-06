722a0b37302e4fefa2786e347aef74fef8410eb7
Daiji NISHIMURA <bundai223@gmail.com>
2022-06-06 18:37:46 +0900

    docker 対応中

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -1,38 +1,40 @@
 include_recipe 'dependency.rb'
 
 dotfile_repos = node[:dotfile_repos]
-docker_compose_version = '1.21.2'
+docker_compose_version = '2.6.0'
+
+home = node[:home]
+user = node[:user]
+group = node[:group]
 
 case node[:platform]
 when 'debian', 'mint'
 when 'ubuntu'
-  # wslはなにもしない
-  unless node[:is_wsl]
-    # TODO: install docker
-  end
 
+  directory "#{home}/.docker/cli-plugins" do
+    owner user
+    group group
+    mode '755'
+  end
   execute 'install docker-compose' do
     command <<-EOL
-      curl -L https://github.com/docker/compose/releases/download/#{docker_compose_version}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-      chmod +x /usr/local/bin/docker-compose
+      compose_path=~/.docker/cli-plugins/docker-compose
+      curl -L https://github.com/docker/compose/releases/download/v#{docker_compose_version}/docker-compose-$(uname -s)-$(uname -m) -o ${compose_path}
+      chmod +x ${compose_path}
     EOL
-    not_if 'which docker-compose'
+    user user
+    # not_if 'which docker-compose'
   end
 
 when 'fedora', 'redhat', 'amazon'
-  package 'yum-utils'
-  package 'device-mapper-persistent-data'
-  package 'lvm2'
-
-  execute 'yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo'
-  package 'docker-ce'
-  package 'docker-ce-cli'
-  package 'containerd.io'
+  # https://matsuand.github.io/docs.docker.jp.onthefly/engine/security/rootless/
 
 when 'osx', 'darwin'
 
 when 'arch'
-  yay 'docker'
+  # yay 'docker'
+  # https://matsuand.github.io/docs.docker.jp.onthefly/engine/security/rootless/
+
   yay 'docker-compose'
 when 'opensuse'
 else
@@ -48,9 +50,5 @@ end
 
 execute "update docker's zsh completions." do
   command "curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > #{dotfile_repos}/config/zsh/functions/completions/_docker"
-end
-
-execute "update docker-compose's zsh completions." do
-  command "curl -L https://raw.githubusercontent.com/docker/compose/#{docker_compose_version}/contrib/completion/zsh/_docker-compose> #{dotfile_repos}/config/zsh/functions/completions/_docker-compose"
 end
 
