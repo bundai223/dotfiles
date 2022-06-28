b86c9c910cbaad394e2746d037cdff03cf467511
bundai223 <bundai223@gmail.com>
2022-06-28 18:42:10 +0900

    messagesに全出力する設定？

--- config/.config/nvim/lua/plugins.lua
+++ config/.config/nvim/lua/plugins.lua
@@ -346,6 +346,18 @@ return require('packer').startup(function(use)
     after = { "cmp-nvim-lsp" },
     config = function()
       -- require("rc/pluginconfig/nvim-lspconfig")
+      -- https://github.com/Pocco81/TheSupercalifragilisticexpialidociousDots/blob/main/.config/nvim/lua/nvdope/initialization/lsp/lspconfig.lua#L97-107
+      -- suppress error messages from lang servers
+      vim.notify = function(msg, log_level, opts)
+        if msg:match("exit code") then
+          return
+        end
+        if log_level == vim.log.levels.ERROR then
+          vim.api.nvim_err_writeln(msg)
+        else
+          vim.api.nvim_echo({{msg}}, true, {})
+        end
+      end
 
       -- [Neovim builtin LSP設定入門](https://zenn.dev/nazo6/articles/c2f16b07798bab)
       vim.api.nvim_set_keymap("n", "<leader>ld", "<Cmd>Trouble<CR>", { noremap = true })
