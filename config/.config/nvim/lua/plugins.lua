558073b485543eb6656bdc7a98328a0ce60a14d5
bundai223 <bundai223@gmail.com>
2022-06-28 10:59:16 +0900

    nvim-cmpのbinding

--- config/.config/nvim/lua/plugins.lua
+++ config/.config/nvim/lua/plugins.lua
@@ -79,6 +79,16 @@ return require('packer').startup(function(use)
       vim.g.completeopt = "menu,menuone,noselect"
 
       local cmp = require'cmp'
+      local types = require("cmp.types")
+      -- local luasnip = require("luasnip")
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+      local t = function(str)
+        return vim.api.nvim_replace_termcodes(str, true, true, true)
+      end
+
       -- Global setup.
       cmp.setup({
         formatting = {
@@ -118,12 +128,68 @@ return require('packer').startup(function(use)
           -- completion = cmp.config.window.bordered(),
           -- documentation = cmp.config.window.bordered(),
         },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
+        mapping = {
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+          ["<Up>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              vim.api.nvim_feedkeys(t("<Up>"), "n", true)
+            end
+          end, { "i" }),
+          ["<Down>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              vim.api.nvim_feedkeys(t("<Down>"), "n", true)
+            end
+          end, { "i" }),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+              -- elseif luasnip.expand_or_jumpable() then
+              -- 	luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            -- elseif luasnip.jumpable(-1) then
+            --   luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<C-Down>"] = cmp.mapping(function(fallback)
+            fallback()
+            -- if luasnip.expand_or_jumpable() then
+            --   luasnip.expand_or_jump()
+            -- else
+            --   fallback()
+            -- end
+          end, { "i", "s" }),
+
+          ["<C-Up>"] = cmp.mapping(function(fallback)
+            fallback()
+            -- if luasnip.jumpable(-1) then
+            --   luasnip.jump(-1)
+            -- else
+            --   fallback()
+            -- end
+          end, { "i", "s" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+          ["<C-q>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        },
         -- sources = cmp.config.sources({
         --   { name = 'nvim_lsp', priority = 100 },
         --   { name = 'vsnip' }, -- For vsnip users.
@@ -280,6 +346,10 @@ return require('packer').startup(function(use)
     after = { "cmp-nvim-lsp" },
     config = function()
       -- require("rc/pluginconfig/nvim-lspconfig")
+
+      -- [Neovim builtin LSP設定入門](https://zenn.dev/nazo6/articles/c2f16b07798bab)
+      vim.api.nvim_set_keymap("n", "<leader>ld", "<Cmd>Trouble<CR>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<leader>lK", "<Cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true })
     end,
   })
   use({
@@ -739,35 +809,35 @@ return require('packer').startup(function(use)
 
           local null_ls = require("null-ls")
 
-          local function file_exists(fname)
-            local stat = vim.loop.fs_stat(vim.fn.expand(fname))
-            return (stat and stat.type) or false
-          end
-          local ignored_filetypes = {
-            "TelescopePrompt",
-            "diff",
-            "gitcommit",
-            "unite",
-            "qf",
-            "help",
-            "markdown",
-            "minimap",
-            "packer",
-            "dashboard",
-            "telescope",
-            "lsp-installer",
-            "lspinfo",
-            "NeogitCommitMessage",
-            "NeogitCommitView",
-            "NeogitGitCommandHistory",
-            "NeogitLogView",
-            "NeogitNotification",
-            "NeogitPopup",
-            "NeogitStatus",
-            "NeogitStatusNew",
-            "aerial",
-            "null-ls-info",
-          }
+          -- local function file_exists(fname)
+          --   local stat = vim.loop.fs_stat(vim.fn.expand(fname))
+          --   return (stat and stat.type) or false
+          -- end
+          -- local ignored_filetypes = {
+          --   "TelescopePrompt",
+          --   "diff",
+          --   "gitcommit",
+          --   "unite",
+          --   "qf",
+          --   "help",
+          --   "markdown",
+          --   "minimap",
+          --   "packer",
+          --   "dashboard",
+          --   "telescope",
+          --   "lsp-installer",
+          --   "lspinfo",
+          --   "NeogitCommitMessage",
+          --   "NeogitCommitView",
+          --   "NeogitGitCommandHistory",
+          --   "NeogitLogView",
+          --   "NeogitNotification",
+          --   "NeogitPopup",
+          --   "NeogitStatus",
+          --   "NeogitStatusNew",
+          --   "aerial",
+          --   "null-ls-info",
+          -- }
 
           local sources = {
             null_ls.builtins.diagnostics.cfn_lint,
