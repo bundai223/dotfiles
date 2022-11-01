9d84ddb3f011659f37f2e5bda52b25d7333a4af3
bundai223 <bundai223@gmail.com>
2022-11-01 10:45:24 +0900

    CFnのschemaをちょっと直したやつに

--- config/.config/nvim/lua/plugins.lua
+++ config/.config/nvim/lua/plugins.lua
@@ -433,6 +433,7 @@ return require('packer').startup(function(use)
                 -- https://www.schemastore.org/json/
                 schemas = {
                   -- ["AWS CloudFormation"] = { "*.cf.{yml,yaml}", "*.{yml,yaml}", "cloud*formation/*.{yml,yaml}" },
+                  ["/home/nishimura/repos/github.com/bundai223/goformation/schema/cloudformation.schema.json"] = { "*.cf.{yml,yaml}", "*.{yml,yaml}", "cloud*formation/*.{yml,yaml}" },
                   ["docker-compose.yml"] = { "docker-compose.{yml,yaml}", "docker-compose*.{yml,yaml}" },
                   -- ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = { "docker-compose.yml", "docker-compose*.yml" },
                   ["gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
