9de1204839a21b39bd7f58222ee1da6076a6964f
bundai223 <bundai223@gmail.com>
2023-02-09 17:50:46 +0900

    terraform用の設定調整

---
+++ cookbooks/terraform/default.rb
@@ -0,0 +1,23 @@
+# https://developer.hashicorp.com/terraform/downloads
+include_recipe 'dependency.rb'
+
+case node[:platform]
+when 'arch'
+  raise NotImplementedError
+when 'osx', 'darwin'
+  raise NotImplementedError
+when 'fedora', 'redhat', 'amazon'
+  raise NotImplementedError
+when 'debian', 'ubuntu', 'mint'
+  execute 'install terraform' do
+    command <<~EOCMD
+      wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+      echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+      sudo apt update -y && sudo apt install -y terraform
+    EOCMD
+  end
+when 'opensuse'
+  raise NotImplementedError
+else
+  raise NotImplementedError
+end
