9dd1bd96f283618cbf4e0c3f907e75a55e770029
bundai223 <bundai223@gmail.com>
2023-03-16 18:37:41 +0900

    update

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -10,21 +10,21 @@ local launch_menu = {}
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   -- Main Wsl
   table.insert(launch_menu, {
-      label = 'Ubuntu 22.04',
-      -- args = { 'wsl.exe', '~ -d Ubuntu-22.04 --' }
-      args = { 'wsl.exe', '~' }
+    label = 'Ubuntu 22.04',
+    -- args = { 'wsl.exe', '~ -d Ubuntu-22.04 --' }
+    args = { 'wsl.exe', '~' }
   })
 
   -- Main Powershell
   table.insert(launch_menu, {
-      label = 'PowerShell',
-      args = { 'pwsh.exe' },
+    label = 'PowerShell',
+    args = { 'pwsh.exe' },
   })
 
   -- Old Powershell(緊急時用)
   table.insert(launch_menu, {
-      label = 'Old PowerShell',
-      args = { 'powershell.exe', '-NoLogo' },
+    label = 'Old PowerShell',
+    args = { 'powershell.exe', '-NoLogo' },
   })
 
 
@@ -32,25 +32,25 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   -- environment command prompts to the menu
   for _, vsvers in
   ipairs(
-      wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
+    wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
   )
   do
     local year = vsvers:gsub('Microsoft Visual Studio/', '')
     table.insert(launch_menu, {
-        label = 'x64 Native Tools VS ' .. year,
-        args = {
-            'cmd.exe',
-            '/k',
-            'C:/Program Files (x86)/'
-            .. vsvers
-            .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
-        },
+      label = 'x64 Native Tools VS ' .. year,
+      args = {
+        'cmd.exe',
+        '/k',
+        'C:/Program Files (x86)/'
+        .. vsvers
+        .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
+      },
     })
   end
 else
   table.insert(launch_menu, {
-      label = 'Zsh',
-      args = { 'zsh' }
+    label = 'Zsh',
+    args = { 'zsh' }
   })
 end
 ------------------------------------------
@@ -71,29 +71,29 @@ default_prog = launch_menu[1].args
 -- default_prog = { 'wsl.exe', '-d Ubuntu-22.04'}
 
 return {
-    ------------------------------------------
-    -- General
-    default_cwd = '~',
-    -- wsl_domains = wsl_domains,
-    default_prog = default_prog,
-    launch_menu = launch_menu,
-    use_ime = true,
-    ------------------------------------------
-    -- Appearance
-    -- font = wezterm.font("HackGenNerd Console"), -- 自分の好きなフォントいれる
-    -- font = wezterm.font("Fira Code"), -- 自分の好きなフォントいれる
-    font = wezterm.font_with_fallback {
-        'Fira Code',
-        "HackGenNerd Console",
-    },
-    font_size = 12.0,
-    color_scheme = "iceberg-dark", -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
-    hide_tab_bar_if_only_one_tab = false,
-    adjust_window_size_when_changing_font_size = false,
-    ------------------------------------------
-    -- Key Binding
-    -- timeout_milliseconds defaults to 1000 and can be omitted
-    leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 1001 },
-    keys = keybinds.create_keybinds(),
-    key_tables = keybinds.key_tables,
+  ------------------------------------------
+  -- General
+  default_cwd = '~',
+  -- wsl_domains = wsl_domains,
+  default_prog = default_prog,
+  launch_menu = launch_menu,
+  use_ime = true,
+  ------------------------------------------
+  -- Appearance
+  -- font = wezterm.font("HackGenNerd Console"), -- 自分の好きなフォントいれる
+  -- font = wezterm.font("Fira Code"), -- 自分の好きなフォントいれる
+  font = wezterm.font_with_fallback {
+    'Fira Code',
+    "HackGenNerd Console",
+  },
+  font_size = 12.0,
+  color_scheme = "iceberg-dark",   -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
+  hide_tab_bar_if_only_one_tab = false,
+  adjust_window_size_when_changing_font_size = false,
+  ------------------------------------------
+  -- Key Binding
+  -- timeout_milliseconds defaults to 1000 and can be omitted
+  leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 1001 },
+  keys = keybinds.create_keybinds(),
+  key_tables = keybinds.key_tables,
 }
