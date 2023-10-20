49131ab5d4e92eb4006fe7cb4d104d64f0af828a
bundai223 <bundai223@gmail.com>
2023-10-20 18:53:07 +0900

    update

--- setup.ps1
+++ setup.ps1
@@ -3,9 +3,12 @@ $ErrorActionPreference = 'Stop'
 # windows powershellじゃないと動かなさそう
 Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 
-winget install --silent Microsoft.PowerToys Microsoft.WindowsTerminal Microsoft.VisualStudioCode Amazon.NoSQLWorkbench
+winget install --silent Microsoft.PowerToys Microsoft.VisualStudioCode Amazon.NoSQLWorkbench
 winget install --silent Buttercup.Buttercup KeeWeb.KeeWeb win32yank
 winget install JanDeDobbeleer.OhMyPosh -s winget
+winget install --silent XPFCXLSGH2H786 # msstore CorvusSKK
+winget install --silent 9N0DX20HK701 # msstore Windows Terminal
+winget install --silent 9N3GRQTX1QPF # Screenpresso
 
 wsl --install 
 
