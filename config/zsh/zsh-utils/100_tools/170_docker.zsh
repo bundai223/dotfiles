6769a7f964bf55e260d4e0c5735e1ea21aad776c
bundai223 <bundai223@gmail.com>
2023-11-30 20:37:57 +0900

    update

--- config/zsh/zsh-utils/100_tools/170_docker.zsh
+++ config/zsh/zsh-utils/100_tools/170_docker.zsh
@@ -5,7 +5,7 @@ dcrun () {
   service_name=$1
   shift
 
-  if [ -z "$(docker compose ps | tail +2 | grep " $service_name " | grep ' running ')" ]; then
+  if [ -z "$(docker compose ps $service_name --format "{{ .Status }}" --filter "status=running")" ]; then
     docker compose run --rm $service_name $@
   else
     # execのときはクオート不要だった
